1b0341d97322ebe3713191fcd8c39d950ea1a19a
Zhi Ping <jenzp21@gmail.com>
2025-03-07 17:42:20 +0800

    Added hash sync.

--- server.js
+++ server.js
@@ -18,18 +18,13 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     });
 });
 
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
-    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
-        console.log(res);
-    });
-});
-
-
 //END_ASYNC
 
 //START_SYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
