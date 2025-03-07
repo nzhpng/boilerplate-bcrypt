d0a7c6b51a1fc2be017078afe6510c742dcb2026
Zhi Ping <jenzp21@gmail.com>
2025-03-07 17:37:01 +0800

    Added hashing function.

--- server.js
+++ server.js
@@ -11,7 +11,19 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+    });
+});
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+    });
+});
 
 
 //END_ASYNC
